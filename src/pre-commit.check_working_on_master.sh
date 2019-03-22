0f07d27b6e580a6f6f610ce6e9a90db8a90bd72e
yashuk803 <yashuk803@gmail.com>
2019-03-22 14:27:31 +0200

    bash script check working on the master branch

---
+++ src/pre-commit.check_working_on_master.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+BRANCH_MASTER="master";
+
+if [[ $(git rev-parse --abbrev-ref HEAD) == $BRANCH_MASTER ]];
+then
+	echo "!!!WARNING!!! Now you working on the master branch."
+while :
+do
+	read -p "Are you really want to continue?  [y/n] " answer
+	case "$answer" in
+		y|yes)
+			echo -e  "Commit successful."
+			exit 0
+			;;
+		n|no)
+			echo -e "Commit has failed."
+			exit 1
+			;;
+		*)
+			echo "Enter something y|yes or n|no."
+			;;
+	esac
+done
+fi
