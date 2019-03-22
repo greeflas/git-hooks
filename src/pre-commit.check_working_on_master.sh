ec16cd05d645481ac501768867d98aa365b698d5
yashuk803 <yashuk803@gmail.com>
2019-03-22 17:50:30 +0200

    add color for message warning

--- src/pre-commit.check_working_on_master.sh
+++ src/pre-commit.check_working_on_master.sh
@@ -3,10 +3,12 @@
 set -e
 
 BRANCH_MASTER="master"
+YELLOW='\033[0;33m'
+CO='\033[0m'
 
 if [[ $(git rev-parse --abbrev-ref HEAD) == $BRANCH_MASTER ]];
 then
-    echo "!!!WARNING!!! Now you working on the master branch."
+    printf "\n$YELLOW!!!WARNING!!! Now you are working on the master branch.\n$CO"
     exec < /dev/tty
     while :
     do
