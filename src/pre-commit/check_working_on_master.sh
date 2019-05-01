f924cab8a3bc481b8813d8c20fb7c5f09eeb15ef
greeflas <vldmr.kuprienko@gmail.com>
2019-05-01 10:42:50 +0300

    Fixes spellchecker error

--- src/pre-commit/check_working_on_master.sh
+++ src/pre-commit/check_working_on_master.sh
@@ -2,20 +2,22 @@
 
 set -e
 
-BRANCH_MASTER="master"
+BRANCH_MASTER='master'
 YELLOW='\033[0;33m'
 CO='\033[0m'
 
-if [[ $(git rev-parse --abbrev-ref HEAD) == $BRANCH_MASTER ]];
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ $current_branch == "$BRANCH_MASTER" ]];
 then
-    printf "\n$YELLOW!!!WARNING!!! Now you are working on the master branch.\n$CO"
+    printf "\n$%s!!!WARNING!!! Now you are working on the master branch.\n%s" "$YELLOW" "$CO"
     exec < /dev/tty
     while :
     do
-        read -p "Are you really want to continue?  [y/n] " answer
+        read -pr "Are you really want to continue?  [y/n] " answer
         case "$answer" in
             y|yes)
-                echo "Commited successfully.\n"
+                printf "Commited successfully.\n"
                 exit 0
                 ;;
             n|no)
