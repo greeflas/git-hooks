abf4d35552bdf15e8c9bd70c5ff63d31396cb254
maxim-kravets <kravets.development@gmail.com>
2019-05-09 20:59:44 +0300

    Fix code style

--- src/post-merge/composer_install.sh
+++ src/post-merge/composer_install.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-composer -v > /dev/null 2>&1
+command -v composer > /dev/null 2>&1
 COMPOSER=$?
 
-if [[ $COMPOSER -ne 0 ]]; then
+if [[ $COMPOSER -ne 0 ]];
+then
     echo "Composer is not installed!"
     exit 1
 fi
